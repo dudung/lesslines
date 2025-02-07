5fbb6bac33c331aa551fe4e960b69a93a7f30a0d
Sparisoma Viridi <dudung@gmail.com>
2025-02-07 16:01:27 +0700

    neew

---
+++ src/lesslines/data/observed/physical_pendulum.py
@@ -0,0 +1,96 @@
+data_00 =
+{
+    "source": {
+        "authors": [
+            "Nani Yuningsih", "Sardjito Sardjito", "Yunita Citra Dewi"
+        ],
+        "title": "Determination of Earth's Gravitational Acceleration and Moment of Inertia of Rigid Body Using Physical Pendulum Experiments",
+        "journal": "IOP Conference Series: Materials Science and Engineering",
+        "volume": "830",
+        "number": "2",
+        "pages": "022001",
+        "month": "May",
+        "year": 2020,
+        "url": "https://doi.org/10.1088/1757-899X/830/2/022001"
+    },
+    "data": {
+        "distance": {
+            "values": [0.350, 0.325, 0.300, 0.275, 0.250, 0.200, 0.188, 0.175, 0.150, 0.125, 0.100, 0.075, 0.050],
+            "unit": "m"
+        },
+        "period": {
+            "values": [1.360, 1.381, 1.350, 1.333, 1.330, 1.320, 1.332, 1.342, 1.362, 1.415, 1.523, 1.725, 1.990],
+            "unit": "s"
+        },
+        "angle": {
+            "value": 10,
+            "unit": "degree"
+        },
+        "mass": {
+            "value": 0.465,
+            "unit": "kg"
+        },
+        "length": {
+            "value": 0.75,
+            "unit": "m"
+        }
+    },
+    "others": {
+        "camcorder": {
+            "type": "Panasonic HDC-SD9",
+            "fps": 25,
+            "observation": {
+                "duration": 50,
+                "unit": "oscillation"
+            }
+        }
+    }
+}
+
+data_01 =
+{
+    "source": {
+        "authors": [
+            "Mustafa Coramik", "Buket İnanç"
+        ],
+        "title": "A physical pendulum experiment with Lego, Phyphox and Tracker",
+        "journal": "Physics Education",
+        "volume": "58",
+        "number": "5",
+        "pages": "055014",
+        "month": "July",
+        "year": 2023,
+        "url": "http://dx.doi.org/10.1088/1361-6552/ace57d"
+    },
+    "data": {
+        "total_length": {
+            "values": [48.00, 52.80, 57.60, 62.40, 67.20],
+            "unit": "cm"
+        },
+        "angle": {
+            "values": [13.8, 15.5, 12.8, 9.4, 9.5],
+            "unit": "degree"
+        },
+        "number_of_brick": {
+            "values": [50, 55, 60, 65, 70],
+            "unit": ""
+        },
+        "repetion": {
+            "values": [5, 5, 5, 5, 5],
+            "unit": ""
+        },
+        "period_tracker": {
+            "values": [1.139, 1.199, 1.249, 1.300, 1.349],
+            "unit": "s"
+        },
+        "period_phyphox": {
+            "values": [1.140, 1.199, 1.258, 1.300, 1.340],
+            "unit": "s"
+        }
+    },
+    "others": {
+        "smartphone": {
+            "fps": 60
+        }
+    }
+}
