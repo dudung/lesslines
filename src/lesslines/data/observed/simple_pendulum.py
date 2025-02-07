54a1ae0f65749cbee01ec8085ec3c38801227f30
Sparisoma Viridi <dudung@gmail.com>
2025-02-07 21:18:33 +0700

    new, data+2

---
+++ src/lesslines/data/observed/simple_pendulum.py
@@ -0,0 +1,73 @@
+data = [d00, d01]
+
+d01 =
+{
+    "source": {
+        "authors": [
+            "Robert A. Nelson", "M. G. Olsson"
+        ],
+        "title": "The pendulum—Rich physics from a simple system",
+        "journal": "American Journal of Physics",
+        "volume": "54",
+        "number": "2",
+        "pages": "112-121",
+        "month": "February",
+        "year": 1986,
+        "url": "https://doi.org/10.1119/1.14703"
+    },
+    "data": {
+       "total_time": {
+            "values": [347.84, 347.87, 347.86, 347.91, 347.91, 347.94, 347.84, 347.81, 347.84, 347.98],
+            "unit": "s",
+            "number_of_cycles": 100
+        },
+        "cord_length": {
+            "value": 3.0044,
+            "unit": "m"
+            "uncertainty": 0.0003
+        },
+        "ball_diameter": {
+            "value": 6.10,
+            "unit": "cm",
+            "uncertainty": 0.01
+        },
+        "ball_mass": {
+            "value": 856.7,
+            "unit": "g",
+            "uncertainty": 0.1
+        }
+    }
+}
+
+d00 =
+{
+    "source": {
+        "authors": [
+            "Mawuadem K. Amedeker"
+        ],
+        "title": "Simple Pendulum Experiment: Angular Approximation Revisited",
+        "journal": "European Journal of Education and Pedagogy",
+        "volume": "3",
+        "number": "3",
+        "pages": "33-35",
+        "month": "May",
+        "year": 2022,
+        "url": "http://doi.org/10.24018/ejedu.2022.3.3.248"
+    },
+    "data": {
+        "angle": {
+            "values": [5, 10, 15, 20, 25],
+            "unit": "degree"
+        },
+        "total_time_1": {
+            "values": [37.98, 38.14, 38.58, 39.29, 40.05],
+            "unit": "s",
+            "number_of_cycles": 20
+        },
+        "total_time_2": {
+            "values": [37.95, 38.26, 38.70, 39.15, 39.91],
+            "unit": "s",
+            "number_of_cycles": 20
+        }
+    }
+}
